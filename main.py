1ac5157fc59e552c6a9dcad3a860773c64c2ba82
Jag Mort <jagmort@gmail.com>
2024-01-22 11:21:27 +0300

    save to csv

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-import csv
+from datetime import datetime
 import random
 from time import sleep
 
 from bs4 import BeautifulSoup
-import lxml
+import numpy as np
 import requests
 
 
@@ -19,23 +19,31 @@ url = 'https://neolex.iling.spb.ru/search?search_api_fulltext=&page={page}'
 count = 0
 page = 0
 
+data = [['Name', 'Links', 'Themes']]
+
 while count < 10:
 
-    print('Page: ', page)
+    print('Page:', page)
     response = requests.get(url.format(page=page), HEADERS)
     soup = BeautifulSoup(response.text, 'lxml')
-
-    # test = {}
 
     tables = soup.find_all('div', {'class': 'entry-grid-row views-row'})
     for table in tables:
         name = table.find('span', {
-            'class': 'field field--name-title field--type-string field--label-hidden'}).text
-        links = [l.text for l in table.find('div', {'class': 'sense-references'}).find_all(
+            'class': 'field field--name-title field--type-string field--label-hidden'}).text.strip()
+        links = [l.text.strip() for l in table.find('div', {'class': 'sense-references'}).find_all(
             'span', {'class': 'field field--name-title field--type-string field--label-hidden'})]
-        themes = table.find('span', {'class': 'border badge'}).text
+        themes = table.find('span', {'class': 'border badge'}).text.strip()
         print(name, links, themes)
+
+        data.append([name, ','.join(links), themes])
         count += 1
 
     sleep(random.randrange(2, 5))
     page += 1
+
+filename = datetime.now().strftime('%Y%m%d_%H%M%S.csv')
+np.savetxt(filename,
+           data,
+           delimiter=";",
+           fmt='%s')
